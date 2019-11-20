e13515e407349a7b9465067028f9d4a2142b9128
Maks <maksym100@gmail.com>
2019-11-20 19:16:40 +0100

    Udemy-49 Inline scoped CSS
    It is possible to define json with css properties and use as inline style
    
    This way it will remain inlined in resulted page.

--- src/App.js
+++ src/App.js
@@ -12,23 +12,36 @@ class App extends Component {
   }
 
   switchNameHandler = (newName) => {
-    const newState = {... this.state}
+    const newState = {...this.state}
     newState.persons[0].name = newName
     this.setState(newState);
   }
 
   nameCHangedHandler = (event) => {
-    const newState = {... this.state}
+    const newState = {...this.state}
     newState.persons[0].name = event.target.value
     this.setState(newState);
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App"> 
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler(':o Name has changed')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler(':o Name has changed')}
+        >
+          Switch Name
+        </button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} 
