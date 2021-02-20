c79b72e21d4ae053af40f8df8bc3e80703912f90
Maksym Adamek <maksym100@gmail.com>
2021-02-20 20:32:47 +0100

    Udemy-71 Conditional styling with StyledComponents
    StyledComponents dependency allows for conditional styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import styled from 'styled-components'
+
 import './App.css';
 import Person from './Person/Person'
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
 
@@ -35,52 +51,38 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+    let persons = null;
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+              key={person.id}
+            />
+          })}
+        </div>
+      );
+    }
 
-let persons = null;
-if(this.state.showPersons) {
-  persons = (
-    <div>
-      {this.state.persons.map((person, index) => {
-        return <Person 
-          name={person.name}
-          age={person.age}
-          click={() => this.deletePersonHandler(index)}
-          changed={(event) => this.nameChangedHandler(event, person.id)}
-          key={person.id}
-        />
-      })}
-    </div>
-  );
+    let classes = [];
 
-  style.backgroundColor = 'red';
-}
+    if(this.state.persons.length <= 1) {
+      classes.push('red');
+    }
 
-let classes = [];
-
-if(this.state.persons.length <= 1) {
-  classes.push('red');
-}
-
-if(this.state.persons.length <= 0) {
-  classes.push('bold');
-}
+    if(this.state.persons.length <= 0) {
+      classes.push('bold');
+    }
 
     return (
       <div className="App"> 
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >Toogle Persons</button>
-
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toogle Persons</StyledButton>
         {persons}
       </div>
     );
